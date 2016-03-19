84e6bdbf2d6fc2ad0ac33e3d0a7bd88a0b55046a
Akash Patel <uni.mail.concordia@gmail.com>
2016-03-19 13:06:08 -0400

    Added an empty TestVirus and a Tile class

---
+++ TowerDefense/TowerDefense/Data/Models/Tile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense.Data.Models
+{
+    class Tile: Position
+    {
+        public Position Position { get; set; }
+        public int Type { get; set; } // 0 for empty and 1 for path
+    }
+}
