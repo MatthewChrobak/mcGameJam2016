683726a1a9b589d60e352edac3f651779139fcce
ConnorYoung <connoryoung23@hotmail.com>
2016-03-19 16:34:21 -0400

    added spawn manager
    still in progress

---
+++ TowerDefense/TowerDefense/Data/Models/SpawnManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TowerDefense.Data.Models.Viruses;
+
+namespace TowerDefense.Data.Models
+{
+    public static class SpawnManager
+    {
+        // Spawns a wave of the given virus.
+        public static void spawnWave(int amount, Virus virus)
+        {
+            // reference to the game map.
+            Object[,] map = DataManager.Map.mapArray;
+           // for(int i = 0;)
+        }
+    }
+}
